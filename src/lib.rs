267bf6a0c9b8eae466ea6bff81db9e55e207e6ec
Ziqin Gong <gongzq0301@gmail.com>
2024-03-07 13:28:18 +0800

    First version

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,21 @@
+use std::process;
+
+use anyhow::{Context, Error, Ok, Result};
 use clap::Parser;
+use serde::{Deserialize, Serialize};
+
+fn number_each_item(item: (usize, &String)) -> String {
+    let mut n = format!("{}. ", item.0 + 1);
+    n.push_str(item.1);
+
+    n
+}
 
 #[derive(Parser, Debug)]
 #[command(version)]
 pub struct Args {
     /// Content to be translated.
-    content: String,
+    contents: Vec<String>,
 
     /// Source language code. Default: en
     #[arg(short, long)]
@@ -14,15 +25,100 @@ pub struct Args {
     #[arg(short, long)]
     tgt_lang: Option<String>,
 
-    /// Google Cloud access token. Can be passed via `GCLOUD_ACCESS_TOKEN`.
-    ///
-    /// Can be obtained by `gcloud auth print-access-token`
-    #[arg(long)]
-    access_token: Option<String>,
-
     /// Google Cloud project ID. Can be passed via `GCLOUD_PROJECT_ID`.
     ///
     /// Must have enabled the Translation API.
     #[arg(short, long)]
     project_id: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Body {
+    source_language_code: String,
+    target_language_code: String,
+    contents: Vec<String>,
+    mime_type: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct TranslResponse {
+    translations: Vec<Translation>,
+}
+
+impl TranslResponse {
+    pub fn parse(&self) -> String {
+        let numbered_transl: Vec<String> = self
+            .translations
+            .iter()
+            .map(|x| x.content())
+            .enumerate()
+            .map(|x| number_each_item(x))
+            .collect();
+
+        numbered_transl.join("\n")
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Translation {
+    translated_text: String,
+}
+
+impl Translation {
+    fn content(&self) -> &String {
+        &self.translated_text
+    }
+}
+
+impl Body {
+    pub fn from(args: Args) -> Self {
+        Self {
+            source_language_code: match args.src_lang {
+                Some(code) => code,
+                None => String::from("en"),
+            },
+            target_language_code: match args.tgt_lang {
+                Some(code) => code,
+                None => String::from("zh"),
+            },
+            contents: args.contents,
+            mime_type: String::from("text/plain"),
+        }
+    }
+}
+
+fn gain_access_token() -> Result<String> {
+    let output = process::Command::new("gcloud")
+        .arg("auth")
+        .arg("print-access-token")
+        .output()
+        .with_context(|| "Unable to run `gcloud auth print-access-token`.")?;
+
+    if !output.status.success() {
+        Err(Error::msg(
+            "`gcloud auth print-access-token` executed with failing error code.",
+        ))
+    } else {
+        String::from_utf8(output.stdout).with_context(|| "Unable to decode access token.")
+    }
+}
+
+pub fn prepare_request(args: &Args) -> Result<reqwest::RequestBuilder> {
+    let access_token = gain_access_token()?.trim().to_owned();
+    let project_id = match &args.project_id {
+        Some(id) => id.to_owned(),
+        None => {
+            std::env::var("GCLOUD_PROJECT_ID").with_context(|| "GCLOUD_PROJECT_ID not set.")?
+        }
+    };
+    let post_url =
+        format!("https://translation.googleapis.com/v3/projects/{project_id}:translateText");
+
+    Ok(reqwest::Client::new()
+        .post(post_url)
+        .header("Authorization", format!("Bearer {access_token}"))
+        .header("x-goog-user-project", project_id)
+        .header("Content-Type", "application/json; charset=utf-8"))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,32 @@
+use std::process;
+
 use clap::Parser;
-use gg_transl::Args;
+use gg_transl::{prepare_request, Args, Body, TranslResponse};
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let args = Args::parse();
+    let response = prepare_request(&args)
+        .unwrap_or_else(|e| {
+            eprintln!("\x1b[1;31mError when creating request!\x1b[0m\n{e}");
+            process::exit(1);
+        })
+        .json(&Body::from(args))
+        .send()
+        .await
+        .unwrap_or_else(|e| {
+            eprintln!("\x1b[1;31mCommunication error!\x1b[0m\n{e}");
+            process::exit(1)
+        });
 
-    println!("{args:#?}");
+    let raw_res = response.text().await.unwrap_or_else(|e| {
+        eprintln!("\x1b[1;31mDecode error!\x1b[0m\n{e}");
+        process::exit(2)
+    });
+    let transl_res: TranslResponse = serde_json::from_str(&raw_res).unwrap_or_else(|_| {
+        eprintln!("\x1b[1;31mNo translation returned!\x1b[0m\nRaw output: {raw_res}");
+        process::exit(3)
+    });
+
+    println!("{}", transl_res.parse());
 }
